f9e655fb104454fc1d192d8079300ce2cb540c80
Sergio Alvarez <sergioandresalvarezvargas5@gmail.com>
2024-03-16 17:55:58 -0500

    se agregaron inputs y elif para las funciones de restar, multiplicar y dividir

--- Funciones/main.py
+++ Funciones/main.py
@@ -8,3 +8,25 @@ if operador == 'Sumar':
     v1 = int(v1)
     v2 = int(v2)
     print(operadores.suma(v1,v2))
+
+elif operador == 'Restar':
+    v1 = input('ingrese el primer valor => ')
+    v2 = input('ingrese el segundo valor => ')
+    v1 = int(v1)
+    v2 = int(v2)
+    print(operadores.resta(v1,v2))
+
+elif operador == 'Multiplicar':
+    v1 = input('ingrese el primer valor => ')
+    v2 = input('ingrese el segundo valor => ')
+    v1 = int(v1)
+    v2 = int(v2)
+    print(operadores.multiplicacion(v1,v2))
+
+elif operador == 'Dividir':
+    v1 = input('ingrese el primer valor => ')
+    v2 = input('ingrese el segundo valor => ')
+    v1 = int(v1)
+    v2 = int(v2)
+    print(operadores.divicion(v1,v2))
+ 
