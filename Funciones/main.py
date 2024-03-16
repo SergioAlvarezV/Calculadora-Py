a1a50388196e5e2de3d889e014cfbd5370e38e33
Sergio Alvarez <sergioandresalvarezvargas5@gmail.com>
2024-03-16 17:52:01 -0500

    Se agrego un input y un if para la funcion de sumar dos valores en main.py

--- Funciones/main.py
+++ Funciones/main.py
@@ -1,3 +1,10 @@
 import operadores
 operador = input('Que quieres hacer Sumar, Restar, Multiplicar o Dividir: ')
 operador = operador.capitalize()
+
+if operador == 'Sumar':
+    v1 = input('ingrese el primer valor => ')
+    v2 = input('ingrese el segundo valor => ')
+    v1 = int(v1)
+    v2 = int(v2)
+    print(operadores.suma(v1,v2))
