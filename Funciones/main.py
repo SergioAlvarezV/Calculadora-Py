f440cd7d405d47e91d9c2617c81c0ab4ab29b68a
Sergio Alvarez <sergioandresalvarezvargas5@gmail.com>
2024-03-18 20:05:21 -0500

    Se agrego un ciclo while para repetir las operaciones hasta que le usuario lo permita,  tambien se cambio el nombre a algunas variables

--- Funciones/main.py
+++ Funciones/main.py
@@ -1,35 +1,69 @@
 import operadores
-operador = input('Que quieres hacer Sumar, Restar, Multiplicar o Dividir: ')
-operador = operador.capitalize()
+def user_input():
+  operadores = ('Sumar', 'Restar', 'Multiplicar', 'Dividir')
+  user_options = input('Que quiere hacer Sumar, restar, multiplicar o dividir ')
+  user_options = user_options.capitalize()
+  if user_options not in operadores:
+    print('Esa operador no esta en las opciones')
+    return None,None
+  
+  return user_options
 
-if operador == 'Sumar':
-    v1 = input('ingrese el primer valor => ')
-    v2 = input('ingrese el segundo valor => ')
+def operacioness(next,user_options):
+  if user_options == 'Sumar':
+    v1 = input('ingrese el primer valor: ')
+    v2 = input('ingrese el segundo valor: ')
     v1 = int(v1)
     v2 = int(v2)
-    print(operadores.suma(v1,v2))
-
-elif operador == 'Restar':
-    v1 = input('ingrese el primer valor => ')
-    v2 = input('ingrese el segundo valor => ')
+    print(f'la suma de {v1} + {v2} es', operadores.suma(v1, v2))
+    next = input('Quiere hacer otra opercion Si o No => ')
+    next = str(next)
+    next = next.capitalize()
+     
+  elif user_options == 'Restar':
+    v1 = input('ingrese el primer valor: ')
+    v2 = input('ingrese el segundo valor: ')
     v1 = int(v1)
     v2 = int(v2)
-    print(operadores.resta(v1,v2))
-
-elif operador == 'Multiplicar':
-    v1 = input('ingrese el primer valor => ')
-    v2 = input('ingrese el segundo valor => ')
+    print(f'la resta de {v1} - {v2} es', operadores.resta(v1, v2))
+    next = input('Quiere hacer otra opercion Si o No => ')
+    next = str(next)
+    next = next.capitalize()
+  
+  elif user_options == 'Multiplicar':
+    v1 = input('ingrese el primer valor: ')
+    v2 = input('ingrese el segundo valor: ')
     v1 = int(v1)
     v2 = int(v2)
-    print(operadores.multiplicacion(v1,v2))
-
-elif operador == 'Dividir':
-    v1 = input('ingrese el primer valor => ')
-    v2 = input('ingrese el segundo valor => ')
+    print(operadores.multiplicacion(v1, v2))
+    next = input('Quiere hacer otra opercion Si o No => ')
+    next = str(next)
+    next = next.capitalize()
+  
+  elif user_options == 'Dividir':
+    v1 = input('ingrese el primer valor: ')
+    v2 = input('ingrese el segundo valor: ')
     v1 = int(v1)
     v2 = int(v2)
-    print(operadores.divicion(v1,v2))
+    v3 = int(operadores.divicion(v1, v2))
+    print(v3)
+    next = input('Quiere hacer otra opercion Si o No => ')
+    next = str(next)
+    next = next.capitalize()
+  return next
 
-else:
-    print('No se encuentra ese operador intentalo de nuevo')
+def run():
+  next = "Si"
+  while True:
+    user_options = user_input()
+    next = operacioness(next,user_options)
+    if next == 'No':
+      print('Nos vemos Babay')
+      break
+
+      
+
+
+
+run()
  
